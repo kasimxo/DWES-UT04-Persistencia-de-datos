52c0cf64ce022531f74c35de4fc685ef40de6a9b
kasimxo <kasimxo@gmail.com>
2025-12-27 17:29:46 +0100

    Añadido modelo tarea +  aplicacada migración

--- tarea-planner/tarea_planner/models.py
+++ tarea-planner/tarea_planner/models.py
@@ -8,6 +8,8 @@ Necesitaré:
 - Email
 - Password
 - Rol (alumno o profesor)
+
+Como estas cosas son comunes para ambos, voy a hacer un solo modelo con una distinción de rol
 """
 class User(models.Model):
     id = models.UUIDField(
@@ -26,4 +28,31 @@ class User(models.Model):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f"{self.full_name} ({self.role})"
+        return f"{self.name} {self.surname} ({self.role})"
+
+"""
+Para el modelo de tarea, necesito:
+- Título
+- Descripción
+- Fecha de entrega
+
+Como una tarea puede estar asignada un alumno o a varios,
+haré una relación ManyToMany con el modelo User
+"""
+class Task(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=models.UUIDField, 
+        editable=False
+        )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    assigned_to = models.ManyToManyField(
+        User, 
+        related_name='tasks',
+        blank=True
+        )
+
+    def __str__(self):
+        return f"{self.title} - Assigned to: {', '.join([user.name for user in self.assigned_to.all()])}"
