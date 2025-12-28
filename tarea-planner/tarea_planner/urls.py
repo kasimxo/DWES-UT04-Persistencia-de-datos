0b330599c674f0abd8020151f3c83ff47c75cdda
kasimxo <kasimxo@gmail.com>
2025-12-28 17:35:58 +0100

    Añadido formulario básico de creación de tareas
    Incluye un listado de usuarios a los que asignar la tarea en forma de tabla.
    El listado de usuarios filtra por rol (para asignar la tarea únicamente a alumnos) y excluye al usuario que está creando la tarea <- Esto último puede que haya que retocarlo, si la crea un alumno, se le asigna por defecto a sí mismo? Un alumno puede crear tareas grupales?
    Se puede asignar a un solo usuario o a varios en función de un input de tipo checkbox "grupal"

--- tarea-planner/tarea_planner/urls.py
+++ tarea-planner/tarea_planner/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView, HomeView, TareasView
+from .views import RegisterView, HomeView, TareasView, CreacionTareasView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', HomeView.as_view(), name='home'),
     path('tareas/', TareasView.as_view(), name='tareas'),
+    path('tareas/crear/', CreacionTareasView.as_view(), name='create_tarea'),
 ]
--- tarea-planner/tarea_planner/views.py
+++ tarea-planner/tarea_planner/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
 from django.views import View
 from .models import User
+
+User = get_user_model()
 
 class RegisterView(View):
 
@@ -49,3 +52,26 @@ class TareasView(LoginRequiredMixin, View):
 
     def get(self, request):
         return render(request, "tareas/tareas.html")
+    
+class CreacionTareasView(LoginRequiredMixin, View):
+    
+    def get(self, request):
+        users = User.objects.filter(role='student').exclude(id=request.user.id)
+        return render(
+            request, 
+            "tareas/crear_tarea.html", 
+            {"users": users})
+
+    def post(self, request):
+        """
+        titulo = request.POST.get("titulo")
+        descripcion = request.POST.get("descripcion")
+
+        if not all([titulo, descripcion]):
+            messages.error(request, "Todos los campos son obligatorios.")
+            return render(request, "tareas/crear_tarea.html")
+
+        # Aquí se guardaría la tarea en la base de datos (lógica no implementada)
+        messages.success(request, "Tarea creada con éxito.")
+        """
+        return redirect("tareas")
