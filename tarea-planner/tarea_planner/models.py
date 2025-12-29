afd570a5647f837b94e78cb01ae156bdacea3c55
kasimxo <kasimxo@gmail.com>
2025-12-29 22:40:22 +0100

    Añadidos algunos campos al modelo de tarea + migración

--- tarea-planner/tarea_planner/models.py
+++ tarea-planner/tarea_planner/models.py
@@ -66,6 +66,15 @@ Para el modelo de tarea, necesito:
 
 Como una tarea puede estar asignada un alumno o a varios,
 haré una relación ManyToMany con el modelo User
+
+Voy a meter también un campo de "Respuesta"
+Este campo sólo see podrá rellenar en la "edición" de la tarea por el alumno
+También voy a meter un campo de evaluable, que será un boolean
+Si la tarea es evaluable, una vez sea entregada, le aparecerá al profesor para poder ponerla apta/no apta
+También voy a meter un campo de evaluación, que será nulable (para las tareas no evaluables) o apto/no apto
+Y por último, la fecha de entrega, para que el alumno pueda darla por "finalizada/entregada"
+
+Una tarea finalizada/entregada no se podrá editar por el alumno, sólo se podrá evaluar por el profesor
 """
 class Task(models.Model):
     id = models.UUIDField(
@@ -75,12 +84,25 @@ class Task(models.Model):
         )
     title = models.CharField(max_length=200)
     description = models.TextField()
+    response = models.TextField(blank=True, null=True)
     due_date = models.DateTimeField()
     assigned_to = models.ManyToManyField(
         User, 
         related_name='tasks',
         blank=True
         )
+    is_evaluable = models.BooleanField(default=False)
+    EVALUATION_CHOICES = [
+        ('apto', 'Apto'),
+        ('no_apto', 'No Apto'),
+    ]
+    evaluation = models.CharField(
+        max_length=10, 
+        choices=EVALUATION_CHOICES, 
+        blank=True, 
+        null=True
+        )
+    finished_at = models.DateTimeField(blank=True, null=True)   
 
     def __str__(self):
         return f"{self.title} - Assigned to: {', '.join([user.name for user in self.assigned_to.all()])}"
